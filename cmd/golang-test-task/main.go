10567594191e45b511e2f3f8a86658b6bbc8cf7c
ten00m <ilya.safronov.1989@gmail.com>
2025-11-24 14:06:35 +0300

    initial commit

---
+++ cmd/golang-test-task/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	//TODO: init logger (slog)
+
+	//TODO: init storage (postgresql)
+
+	//TODO: init router (chi)
+
+	//TODO: run http server
+
+	//TODO: init swagger doc
+
+	//TODO: create http endpoints:
+	//
+	//
+	//
+
+	//TODO: add swagger doc for all endpoints
+
+	//TODO: write tests to handlers
+
+	//TODO: write perfomance test
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ten00m/golang-test-task
+
+go 1.25.4
