afbfb3dd399e8a8b9a9bdc72177298a6e74bea7e
ten00m <ilya.safronov.1989@gmail.com>
2025-11-25 11:13:58 +0300

    add logger middleware to router for enhanced logging

--- internal/router/router.go
+++ internal/router/router.go
@@ -5,12 +5,14 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	mwLogger "github.com/ten00m/golang-test-task/internal/http-server/middleware/logger"
 )
 
 func New(log *slog.Logger) chi.Router {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
+	r.Use(mwLogger.New(log))
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
