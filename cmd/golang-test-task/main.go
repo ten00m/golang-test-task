166ffa0236e8e28f54db8b6587536d01674c7c1b
ten00m <ilya.safronov.1989@gmail.com>
2025-11-25 11:14:24 +0300

    refactor main function to initialize logger, storage, and router; add graceful shutdown handling

--- cmd/golang-test-task/main.go
+++ cmd/golang-test-task/main.go
@@ -1,24 +1,73 @@
 package main
 
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/ten00m/golang-test-task/internal/config"
+	"github.com/ten00m/golang-test-task/internal/logger"
+	"github.com/ten00m/golang-test-task/internal/router"
+	"github.com/ten00m/golang-test-task/internal/storage"
+)
+
 func main() {
-	//TODO: init logger (slog)
+	cfg := config.LoadConfig()
 
-	//TODO: init storage (postgresql)
+	log := logger.New(os.Stdout)
 
-	//TODO: init router (chi)
+	defer func() {
+		if r := recover(); r != nil {
+			log.Error("panic recovered", slog.Any("error", r))
+		}
+	}()
 
-	//TODO: run http server
+	log.Info("configuration loaded",
+		slog.String("http_addr", cfg.HTTPServer.Address),
+	)
 
-	//TODO: init swagger doc
+	db, err := storage.New(&cfg.PostgreSQL, log)
+	if err != nil {
+		log.Error("failed to initialize storage", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Error("failed to close storage", slog.String("error", err.Error()))
+		}
+	}()
 
-	//TODO: create http endpoints:
-	//
-	//
-	//
+	r := router.New(log)
 
-	//TODO: add swagger doc for all endpoints
+	srv := &http.Server{
+		Addr:         cfg.HTTPServer.Address,
+		Handler:      r,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
 
-	//TODO: write tests to handlers
+	errorChan := make(chan struct{})
 
-	//TODO: write perfomance test
+	go func() {
+		log.Info("starting http server", slog.String("addr", srv.Addr))
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Error("server is not started with error", err)
+			errorChan <- struct{}{}
+		}
+	}()
+
+	<-errorChan
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("graceful shutdown failed", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	log.Info("server stopped")
 }
